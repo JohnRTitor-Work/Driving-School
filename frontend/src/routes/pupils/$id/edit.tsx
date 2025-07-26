3014b680cbba2d37cb6b9bd5b739677418408f24
Masum Reza <221108133+johnrtitor-work@users.noreply.github.com>
2025-07-26 12:16:53 +0530

    fix bug

--- frontend/src/routes/pupils/$id/edit.tsx
+++ frontend/src/routes/pupils/$id/edit.tsx
@@ -1,4 +1,4 @@
-import EditPupilPage from "@/pages/Pupil/edit-pupil.page";
+import EditPupilPage from "@/pages/Pupils/edit-pupil.page";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/pupils/$id/edit")({
