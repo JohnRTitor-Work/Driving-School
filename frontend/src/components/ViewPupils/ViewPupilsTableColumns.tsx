51175bc4462ef364f3b8a472fc6874ae08d1409f
Masum Reza <221108133+johnrtitor-work@users.noreply.github.com>
2025-07-27 20:22:56 +0530

    enable selecting tables
    from shadcn examples

--- frontend/src/components/ViewPupils/ViewPupilsTableColumns.tsx
+++ frontend/src/components/ViewPupils/ViewPupilsTableColumns.tsx
@@ -16,8 +16,31 @@ import { ConfirmationDialog } from "@/components/ui/confirmation-dialog";
 import { useDeletePupilById } from "@/api/pupil/pupil.mutation";
 import { Link } from "@tanstack/react-router";
 import { MoreHorizontalIcon } from "lucide-react";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export const viewPupilsTableColumns: ColumnDef<PupilInfo>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
   {
     id: "name",
     header: ({ column }) => {
